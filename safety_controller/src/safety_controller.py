9d913025a583efd51f7a6b46c7a29f09010920db
jamerkel <jamerkel@mit.edu>
2023-06-13 20:35:48 +0000

    full stop

--- safety_controller/src/safety_controller.py
+++ safety_controller/src/safety_controller.py
@@ -12,34 +12,24 @@ class SafetyController:
     SCAN_TOPIC = rospy.get_param("safety_controller/scan_topic")
     SAFETY_TOPIC = rospy.get_param("safety_controller/safety_topic")
     SAFE_DISTANCE = rospy.get_param("safety_controller/safe_distance")
-    VELOCITY = 1#rospy.get_param("wall_follower/velocity")
+    VELOCITY = rospy.get_param("safety_controller/velocity")
 
     def __init__(self):
         #Initialize publisher and subscriber data
 
-        self.current_command = None
-        self.current_scan = None
         self.will_crash = False
-
+        
+        self.scan_sub = rospy.Subscriber(self.SCAN_TOPIC, LaserScan, self.check_safety)
         self.safety_pub = rospy.Publisher(self.SAFETY_TOPIC, AckermannDriveStamped, queue_size=10)
 
-    def command_callback(self, msg):
-        #store most recent drive command published
-        self.current_command = msg
-        self.check_safety()
-
-    def scan_callback(self, msg):
-        #store most recent scan published
-        self.current_scan = msg
-        self.check_safety()
-
-    def check_safety(self):
+    def check_safety(self, msg):
         #check if robot will crash
             #TODO
-        dist = np.array(self.current_scan.ranges)
-        angles = np.linspace(self.current_scan.angle_min, self.current_scan.angle_max, num=len(self.current_scan.ranges))
+        dist = np.array(msg.ranges)
+        angles = np.linspace(msg.angle_min, msg.angle_max, num=len(dist))
         d_theta = (angles[-1]-angles[0])/len(angles)
         weight = np.reciprocal(dist)
+        front = np.split(dist, 5)[2]
         
 
         if min(dist) < self.SAFE_DISTANCE:
@@ -50,16 +40,13 @@ class SafetyController:
             #publish override
             override = AckermannDriveStamped()
             override.header.stamp = rospy.Time.now()
-            override.drive.speed = self.VELOCITY
+            override.drive.speed = 0#self.VELOCITY
             override.drive.steering_angle = 0 #TODO calculate avoidance angle
             self.safety_pub.publish(override)
 
 if __name__ == "__main__":
     rospy.init_node('safety_controller')
     safety_controller = SafetyController()
-
-    rospy.Subscriber(safety_controller.DRIVE_TOPIC, AckermannDriveStamped, safety_controller.command_callback)
-    rospy.Subscriber(safety_controller.SCAN_TOPIC, LaserScan, safety_controller.scan_callback)
 
     rospy.spin()
 
